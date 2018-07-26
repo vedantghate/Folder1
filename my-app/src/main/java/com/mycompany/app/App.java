b9860f7a52b1e2c5d006a737971f02fd4fbce1e1
vedantghate <wantedgutsy@gmail.com>
2018-07-26 15:36:11 +0530

    Changed App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello World, this is JARVIS!" );
+        System.out.println( "Hello World, this is JARVIS!" );
     }
 }
+
+This is error !
