83acdc9c9d3df757c77f3741d7c4f826dcf948ce
vedantghate <wantedgutsy@gmail.com>
2018-08-02 12:50:57 +0530

    Changes made by folder1

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, this is dev1 !" );
+        System.out.println( "Hello World, this is folder1 !" );
     }
 }
 
