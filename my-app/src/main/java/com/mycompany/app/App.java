e5ad5feea3c3a2661597092ecedbe0f2a76d980a
anup <anup.gajbhiye@eth.co.in>
2018-08-02 17:16:16 +0530

    commit

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,11 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Hello World, this is deve1 from ameya laptop anup login !" );
-=======
-        System.out.println( "Hello World, this is folder1 !" );
->>>>>>> 798285d83e3f37177a3496c2f6b234e7f2e763a6
     }
 }
 
