44aae0a7b7c2c79421d8049a29d41928587b0a60
vedantghate <wantedgutsy@gmail.com>
2018-08-02 17:20:20 +0530

    Changes made

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, this is folder1 !" );
+        System.out.println( "Hello World, this is folder1 again !" );
     }
 }
 
