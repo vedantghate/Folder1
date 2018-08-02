c3174a45de046b60f0ee730376f0adfa8ae27336
vedantghate <wantedgutsy@gmail.com>
2018-08-02 12:26:57 +0530

    Changes made

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, this is JARVIS yo!" );
+        System.out.println( "Hello World !" );
     }
 }
 
